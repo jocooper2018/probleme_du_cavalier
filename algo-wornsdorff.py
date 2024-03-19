30e24c39f4304aeb4debaafc773424f82dd02ad3
BZHKyli4n <kylian.houedec@etudiant.univ-rennes1.fr>
2024-03-19 11:52:01 +0100

    tromper de fichier

--- algo-wornsdorff.py
+++ algo-wornsdorff.py
@@ -1,67 +1,58 @@
-def is_valid_move(board, x, y, n):
-    """
-    Vérifie si le mouvement (x, y) est valide sur l'échiquier de taille n x n.
-    """
-    return 0 <= x < n and 0 <= y < n and board[x][y] == -1
+from echiquier import Echec
+from globales import *
+import time
+start = time.time()
 
-def get_move_counts(board, x, y, n):
-    """
-    Retourne le nombre de mouvements possibles à partir de la position (x, y) sur l'échiquier de taille n x n.
-    """
-    moves = [(2, 1), (1, 2), (-1, 2), (-2, 1),
-             (-2, -1), (-1, -2), (1, -2), (2, -1)]
+def is_valid_move(board, move, visited):
+    x, y = move
+    return 0 <= x < SIZE and 0 <= y < SIZE and board[x][y] == 0 and (x, y) not in visited
 
-    count = 0
-    for dx, dy in moves:
-        if is_valid_move(board, x + dx, y + dy, n):
-            count += 1
-    return count
+def get_valid_moves(board, x, y, visited):
+    moves = [
+        (x + 1, y + 2), (x + 2, y + 1),
+        (x + 2, y - 1), (x + 1, y - 2),
+        (x - 1, y - 2), (x - 2, y - 1),
+        (x - 2, y + 1), (x - 1, y + 2)
+    ]
+    return [move for move in moves if is_valid_move(board, move, visited)]
 
-def warnsdorff_tour(n):
-    """
-    Résout le problème du tour du cavalier en utilisant l'algorithme de Warnsdorff.
-    """
-    # Initialisation de l'échiquier
-    board = [[-1 for _ in range(n)] for _ in range(n)]
+def get_move_counts(board, x, y, visited):
+    return [(move, len(get_valid_moves(board, move[0], move[1], visited))) for move in get_valid_moves(board, x, y, visited)]
 
-    # Liste des mouvements possibles du cavalier
-    moves = [(2, 1), (1, 2), (-1, 2), (-2, 1),
-             (-2, -1), (-1, -2), (1, -2), (2, -1)]
+def solve_cavalier(board, x, y, visited, move_count):
+    if move_count == SIZE**2:
+        return True
+    for move, _ in sorted(get_move_counts(board, x, y, visited), key=lambda x: x[1]):
+        visited.append(move)
+        board[move[0]][move[1]] = move_count + 1
+        if solve_cavalier(board, move[0], move[1], visited, move_count + 1):
+            return True
+        visited.pop()
+        board[move[0]][move[1]] = 0
+    return False
 
-    # Position initiale
-    x, y = 0, 0
-    move_count = 0
+def print_board(board):
+    for row in board:
+        print(" ".join(map(str, row)))
 
-    # Réalisation du tour
-    while move_count < n*n:
-        board[x][y] = move_count
-        move_count += 1
+def knight_tour():
+    board = [[0] * SIZE for _ in range(SIZE)]
+    start_x, start_y = X, Y  # Start position
+    visited = [(start_x, start_y)]
+    board[start_x][start_y] = 1
+    if solve_cavalier(board, start_x, start_y, visited, 1):
+        print("Solution found:")
+        return board
+    else:
+        print("No solution found")
+        return None
 
-        # Recherche du prochain mouvement valide avec le moins de mouvements possibles
-        next_moves = [(x + dx, y + dy) for dx, dy in moves
-                      if is_valid_move(board, x + dx, y + dy, n)]
-        next_moves.sort(key=lambda pos: get_move_counts(board, pos[0], pos[1], n))
-
-        # Choix du prochain mouvement
-        if next_moves:
-            x, y = next_moves[0]
-        else:
-            break
-
-    return board
-
-# Exemple d'utilisation avec un échiquier de taille 8x8
-tour_solution = warnsdorff_tour(8)
-for row in tour_solution:
-    print(row)
-    
 if __name__ == "__main__":
-    start = time.time()
-    board = warnsdorff_tour()
+    board = knight_tour()
     if board is not None:
         echec = Echec()
-        echec.placerCavalier((0, 0))
-        for i in range(1, SIZE**2+1):
+        echec.placerCavalier((X, Y))
+        for i in range(1, SIZE**2 + 1):
             trouver = False
             x = 0
             while not trouver and x < SIZE:
@@ -72,5 +63,5 @@ if __name__ == "__main__":
                     y += 1
                 x += 1
             echec.deplacerCavalier((x-1, y-1))
-        tempsTotal = time.time()-start
+        tempsTotal = time.time() - start
         echec.afficher(tempsTotal)
